c05760fc5adb1e6dc3567e92d064d23d23e39fc0
ShamailovMax <max24june@gmail.com>
2022-05-19 01:21:07 +0300

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# online-reader-server-django
-Django version of yhe Online Reader backend
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""reader_server URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("", include("main.urls")),  # new
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
